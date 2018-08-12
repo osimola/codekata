23754d572bfdc832fff5f4a63c37bbec32c8b092
Juhani Simola <juhani.simola@iki.fi>
2018-08-12 23:22:50 +0300

    Remove some duplication and play around with GTest typed test

--- medians/tests.cpp
+++ medians/tests.cpp
@@ -1,148 +1,77 @@
 #include <gtest/gtest.h>
 #include <medians.hpp>
 
-TEST(QsortMedian, in_order_even) {
+struct QsortMedian {
+    static int median(int* data, size_t count) {
+        return median_qsort(data, count);
+    }
+};
+
+struct IsortMedian {
+    static int median(int* data, size_t count) {
+        return median_isort(data, count);
+    }
+};
+
+struct IsortFullMedian {
+    static int median(int* data, size_t count) {
+        return median_isort_full(data, count);
+    }
+};
+
+struct RadixMedian {
+    static int median(int* data, size_t count) {
+        return median_radixsort(data, count);
+    }
+};
+
+struct BubbleMedian {
+    static int median(int* data, size_t count) {
+        return median_bubblesort(data, count);
+    }
+};
+
+template <typename T> class MedianTest : public ::testing::Test {};
+
+using MedianTypes = ::testing::Types<QsortMedian, IsortMedian, IsortFullMedian,
+                                     RadixMedian, BubbleMedian>;
+
+TYPED_TEST_CASE(MedianTest, MedianTypes);
+
+TYPED_TEST(MedianTest, in_order_even) {
     int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_qsort(data, 10));
+    ASSERT_EQ(5, TypeParam::median(data, 10));
 }
 
-TEST(QsortMedian, reverse_order_even) {
+TYPED_TEST(MedianTest, reverse_order_even) {
     int data[] = {21, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    ASSERT_EQ(5, median_qsort(data, 10));
+    ASSERT_EQ(5, TypeParam::median(data, 10));
 }
 
-TEST(QsortMedian, in_order_odd) {
+TYPED_TEST(MedianTest, in_order_odd) {
     // AKA direct hit at pivot
     int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_qsort(data, 10));
+    ASSERT_EQ(5, TypeParam::median(data, 10));
 }
 
-TEST(QsortMedian, median_last_even) {
+TYPED_TEST(MedianTest, median_last_even) {
     int data[] = {1, 2, 3, 4, 6, 7, 8, 9, 21, 5};
-    ASSERT_EQ(5, median_qsort(data, 10));
+    ASSERT_EQ(5, TypeParam::median(data, 10));
 }
 
-TEST(QsortMedian, median_last_odd) {
+TYPED_TEST(MedianTest, median_last_odd) {
     int data[] = {1, 2, 3, 4, 6, 7, 8, 21, 5};
-    ASSERT_EQ(5, median_qsort(data, 9));
+    ASSERT_EQ(5, TypeParam::median(data, 9));
 }
 
-TEST(QsortMedian, median_first_even) {
+TYPED_TEST(MedianTest, median_first_even) {
     int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_qsort(data, 10));
+    ASSERT_EQ(5, TypeParam::median(data, 10));
 }
 
-TEST(QsortMedian, median_first_odd) {
+TYPED_TEST(MedianTest, median_first_odd) {
     int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 21};
-    ASSERT_EQ(5, median_qsort(data, 9));
-}
-
-TEST(IsortMedian, in_order_even) {
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_isort(data, 10));
-}
-
-TEST(IsortMedian, reverse_order_even) {
-    int data[] = {21, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    ASSERT_EQ(5, median_isort(data, 10));
-}
-
-TEST(IsortMedian, in_order_odd) {
-    // AKA direct hit at pivot
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_isort(data, 10));
-}
-
-TEST(IsortMedian, median_last_even) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 9, 21, 5};
-    ASSERT_EQ(5, median_isort(data, 10));
-}
-
-TEST(IsortMedian, median_last_odd) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 21, 5};
-    ASSERT_EQ(5, median_isort(data, 9));
-}
-
-TEST(IsortMedian, median_first_even) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_isort(data, 10));
-}
-
-TEST(IsortMedian, median_first_odd) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 21};
-    ASSERT_EQ(5, median_isort(data, 9));
-}
-
-TEST(IsortFullMedian, in_order_even) {
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_isort_full(data, 10));
-}
-
-TEST(IsortFullMedian, reverse_order_even) {
-    int data[] = {21, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    ASSERT_EQ(5, median_isort_full(data, 10));
-}
-
-TEST(IsortFullMedian, in_order_odd) {
-    // AKA direct hit at pivot
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_isort_full(data, 10));
-}
-
-TEST(IsortFullMedian, median_last_even) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 9, 21, 5};
-    ASSERT_EQ(5, median_isort_full(data, 10));
-}
-
-TEST(IsortFullMedian, median_last_odd) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 21, 5};
-    ASSERT_EQ(5, median_isort_full(data, 9));
-}
-
-TEST(IsortFullMedian, median_first_even) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_isort_full(data, 10));
-}
-
-TEST(IsortFullMedian, median_first_odd) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 21};
-    ASSERT_EQ(5, median_isort_full(data, 9));
-}
-
-TEST(RadixsortMedian, in_order_even) {
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_radixsort(data, 10));
-}
-
-TEST(RadixsortMedian, reverse_order_even) {
-    int data[] = {21, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    ASSERT_EQ(5, median_radixsort(data, 10));
-}
-
-TEST(RadixsortMedian, in_order_odd) {
-    // AKA direct hit at pivot
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_radixsort(data, 10));
-}
-
-TEST(RadixsortMedian, median_last_even) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 9, 21, 5};
-    ASSERT_EQ(5, median_radixsort(data, 10));
-}
-
-TEST(RadixsortMedian, median_last_odd) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 21, 5};
-    ASSERT_EQ(5, median_radixsort(data, 9));
-}
-
-TEST(RadixsortMedian, median_first_even) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_radixsort(data, 10));
-}
-
-TEST(RadixsortMedian, median_first_odd) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 21};
-    ASSERT_EQ(5, median_radixsort(data, 9));
+    ASSERT_EQ(5, TypeParam::median(data, 9));
 }
 
 TEST(RadixsortMedian, all_zeros) {
@@ -154,44 +83,6 @@ TEST(RadixsortMedian, all_ones) {
     uint8_t data[] = {1, 1, 1, 1};
     ASSERT_EQ(1, median_radixsort(data, 4));
 }
-
-TEST(BubblesortMedian, in_order_even) {
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_bubblesort(data, 10));
-}
-
-TEST(BubblesortMedian, reverse_order_even) {
-    int data[] = {21, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    ASSERT_EQ(5, median_bubblesort(data, 10));
-}
-
-TEST(BubblesortMedian, in_order_odd) {
-    // AKA direct hit at pivot
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_bubblesort(data, 10));
-}
-
-TEST(BubblesortMedian, median_last_even) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 9, 21, 5};
-    ASSERT_EQ(5, median_bubblesort(data, 10));
-}
-
-TEST(BubblesortMedian, median_last_odd) {
-    int data[] = {1, 2, 3, 4, 6, 7, 8, 21, 5};
-    ASSERT_EQ(5, median_bubblesort(data, 9));
-}
-
-TEST(BubblesortMedian, median_first_even) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 9, 21};
-    ASSERT_EQ(5, median_bubblesort(data, 10));
-}
-
-TEST(BubblesortMedian, median_first_odd) {
-    int data[] = {5, 1, 2, 3, 4, 6, 7, 8, 21};
-    ASSERT_EQ(5, median_bubblesort(data, 9));
-}
-
-
 
 TEST(SlidingWindow, binsearch) {
     uint8_t data[] = {10, 20, 30, 40, 50};
